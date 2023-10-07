177662134eb7be975b1de280b4733472c5ae1bcf
Sam Wallace <samwallace1996@gmail.com>
2023-10-07 01:18:55 +0100

    Added rofi switch.

---
+++ .config/polybar/forest/scripts/rofi-switch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+FILE="$HOME/.config/polybar/forest/scripts/rofi/colors.rasi"
+
+rofi -no-config -no-lazy-grab -show window -modi window -theme ~/.config/polybar/forest/scripts/rofi/launcher.rasi
