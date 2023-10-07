534ac31a1cf3008762ab7a4cc3ad689dcd07fb43
Sam Wallace <samwallace1996@gmail.com>
2023-10-07 01:48:06 +0100

    Added config for polybar rofi.

--- .config/polybar/forest/scripts/rofi-switch.sh
+++ .config/polybar/forest/scripts/rofi-switch.sh
@@ -2,4 +2,4 @@
 
 FILE="$HOME/.config/polybar/forest/scripts/rofi/colors.rasi"
 
-rofi -no-config -no-lazy-grab -show window -modi window -theme ~/.config/polybar/forest/scripts/rofi/launcher.rasi
+rofi -no-lazy-grab -show window -modi window -theme ~/.config/polybar/forest/scripts/rofi/launcher.rasi
