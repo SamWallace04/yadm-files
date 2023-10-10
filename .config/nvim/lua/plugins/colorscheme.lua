943765a2ee51aa90a16efdbef46aadf45d6a9f67
Sam Wallace <samwallace1996@gmail.com>
2023-10-10 21:45:03 +0100

    Changed nvim theme.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -7,17 +7,22 @@ return {
     opts = {
       flavour = "mocha", -- latte, frappe, macchiato, mocha
       no_italic = true,
+      transparent_background = true,
     },
   },
-  { "sekke276/dark_flat.nvim", opts = {
-    transparent = true,
-  } },
+  {
+    "sekke276/dark_flat.nvim",
+    -- For some reason adding opts forces the theme to be dark flat. Can be uncommented when it is in use.
+    --   opts = {
+    --   transparent = true,
+    -- }
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dark_flat",
+      colorscheme = "catppuccin",
     },
   },
 }
