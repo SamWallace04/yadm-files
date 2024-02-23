5ea20435d9738e9095d38946c3908a3a30d6e101
Sam Wallace <samwallace1996@gmail.com>
2024-02-23 14:13:24 +0000

    Updated nvim.

--- .config/nvim/lua/plugins/ident-line.lua
+++ .config/nvim/lua/plugins/ident-line.lua
@@ -14,16 +14,6 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    opts = {
-      space_char_blankline = " ",
-      char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-      },
-    },
+    opts = {},
   },
 }
